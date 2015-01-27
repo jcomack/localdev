93a16a598aeb1655f6e84942cd57b4271b336639
Jimmy Comack <jimcom89@gmail.com>
2015-01-28 00:12:08 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Add some information here at some point descripting what the hell I'm trying to do here.
---
+++ nginx_25012015/config/nginx-start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp /opt/etc/nginx.conf /etc/nginx/nginx.conf
+set -i "s/%fpm-ip%/$FPM_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
+
+exec /usr/sbin/nginx
